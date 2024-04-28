0bf2c3fbc911e4c16d4b090b992e50ba3f765f1f
giselle <yajisk0@gmail.com>
2024-04-28 19:13:14 +0100

    initial setting

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express"
+
+const application = express()
+
+const PORT = 1337
+
+application.get("/ping", (request: Request, response: Response) => {
+  response.send("Pong")
+})
+
+application.listen(PORT, () => {
+  console.log("Server up and running")
+})
